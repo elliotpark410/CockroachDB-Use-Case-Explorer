664ab16eac05f82366560edd1eebc6e3c385de1f
Elliot Park <elliotpark410@gmail.com>
2024-09-04 01:57:16 -0700

    add styling to ProspectForm and Results

--- src/components/ProspectForm.js
+++ src/components/ProspectForm.js
@@ -6,125 +6,129 @@ import {
   Select,
   Checkbox,
   NumberInput,
-  NumberInputStepper,
   NumberInputField,
+  NumberInputStepper,
   NumberIncrementStepper,
   NumberDecrementStepper,
   Textarea,
-  CheckboxGroup,
   SimpleGrid,
   RadioGroup,
   Radio,
-  HStack,
+  Box,
 } from '@chakra-ui/react';
 import { Field, useFormikContext } from 'formik';
+
+const CustomFormControl = ({ children, label, required }) => (
+  <FormControl mb={6}>
+    <FormLabel>
+      {label} {required && <span style={{ color: 'red' }}>*</span>}
+    </FormLabel>
+    {children}
+  </FormControl>
+);
 
 const ProspectForm = () => {
   const { values, setFieldValue } = useFormikContext();
 
   return (
-    <VStack spacing={4} align="stretch">
-      <FormControl isRequired>
-        <FormLabel>Data Workload Type</FormLabel>
-        <Field name="dataWorkloadType" as={Select}>
-          <option value="">Select a data workload type</option>
-          <option value="TRANSACTIONAL">Transactional</option>
-          <option value="ANALYTICAL">Analytical</option>
-          <option value="HYBRID">Hybrid</option>
-          <option value="OTHER">Other</option>
-        </Field>
-      </FormControl>
+      <Box maxW="4xl" mx="auto" bg="white" boxShadow="xl" rounded="lg" overflow="hidden">
+        <VStack spacing={6} align="stretch" p={8}>
+          <CustomFormControl label="Data Workload Type" required>
+            <Field name="dataWorkloadType" as={Select} placeholder="Select a data workload type">
+              <option value="TRANSACTIONAL">Transactional</option>
+              <option value="ANALYTICAL">Analytical</option>
+              <option value="HYBRID">Hybrid</option>
+              <option value="OTHER">Other</option>
+            </Field>
+          </CustomFormControl>
 
-      <FormControl isRequired>
-        <FormLabel>Cloud Provider</FormLabel>
-        <Field name="cloudProvider" as={Select}>
-          <option value="">Select a cloud provider</option>
-          <option value="AWS">AWS</option>
-          <option value="GCP">GCP</option>
-          <option value="AZURE">Azure</option>
-          <option value="MULTI_CLOUD">Multi-cloud</option>
-          <option value="ON_PREMISE">On-premise</option>
-          <option value="HYBRID">Hybrid</option>
-          <option value="OTHER">Other</option>
-        </Field>
-      </FormControl>
+          <CustomFormControl label="Cloud Provider" required>
+            <Field name="cloudProvider" as={Select} placeholder="Select a cloud provider">
+              <option value="AWS">AWS</option>
+              <option value="GCP">GCP</option>
+              <option value="AZURE">Azure</option>
+              <option value="MULTI_CLOUD">Multi-cloud</option>
+              <option value="ON_PREMISE">On-premise</option>
+              <option value="HYBRID">Hybrid</option>
+              <option value="OTHER">Other</option>
+            </Field>
+          </CustomFormControl>
 
-      <FormControl>
-        <FormLabel>Tech Stack</FormLabel>
-        <Field name="techStack">
-          {({ field }) => (
-            <CheckboxGroup
-              {...field}
-              value={values.techStack || []}
-              onChange={(values) => setFieldValue('techStack', values)}
-            >
-              <SimpleGrid columns={3} spacing={2}>
-                {['MYSQL', 'POSTGRES', 'ORACLE', 'SQL_SERVER', 'MARIA_DB', 'AURORA', 'DYNAMO_DB', 'SPANNER', 'TERADATA', 'COUCHBASE', 'MONGO_DB', 'IBM_DB2', 'CASSANDRA', 'ELASTICSEARCH', 'REDIS', 'KAFKA', 'KUBERNETES', 'DOCKER', 'OTHER'].map((tech) => (
-                  <Checkbox key={tech} value={tech}>
-                    {tech.replace('_', ' ')}
-                  </Checkbox>
-                ))}
-              </SimpleGrid>
-            </CheckboxGroup>
-          )}
-        </Field>
-      </FormControl>
+          <CustomFormControl label="Tech Stack">
+            <Field name="techStack">
+              {({ field }) => (
+                <SimpleGrid columns={[2, 3, 4]} spacing={4}>
+                  {['MYSQL', 'POSTGRES', 'ORACLE', 'SQL_SERVER', 'MARIA_DB', 'AURORA', 'DYNAMO_DB', 'SPANNER', 'TERADATA', 'COUCHBASE', 'MONGO_DB', 'IBM_DB2', 'CASSANDRA', 'ELASTICSEARCH', 'REDIS', 'KAFKA', 'KUBERNETES', 'DOCKER', 'OTHER'].map((tech) => (
+                    <Checkbox
+                      key={tech}
+                      {...field}
+                      value={tech}
+                      isChecked={field.value && field.value.includes(tech)}
+                      onChange={(e) => {
+                        const set = new Set(field.value || []);
+                        if (e.target.checked) {
+                          set.add(tech);
+                        } else {
+                          set.delete(tech);
+                        }
+                        setFieldValue('techStack', Array.from(set));
+                      }}
+                    >
+                      {tech.replace('_', ' ')}
+                    </Checkbox>
+                  ))}
+                </SimpleGrid>
+              )}
+            </Field>
+          </CustomFormControl>
 
-      <FormControl isRequired>
-        <FormLabel>Data Volume</FormLabel>
-        <Field name="dataVolume" as={Select}>
-          <option value="">Select data volume</option>
-          <option value="0-100GB">0-100GB</option>
-          <option value="100GB-1TB">100GB-1TB</option>
-          <option value="1TB-10TB">1TB-10TB</option>
-          <option value="10TB+">10TB+</option>
-        </Field>
-      </FormControl>
+          <CustomFormControl label="Data Volume" required>
+            <Field name="dataVolume" as={Select} placeholder="Select data volume">
+              <option value="0-100GB">0-100GB</option>
+              <option value="100GB-1TB">100GB-1TB</option>
+              <option value="1TB-10TB">1TB-10TB</option>
+              <option value="10TB+">10TB+</option>
+            </Field>
+          </CustomFormControl>
 
-      <FormControl>
-        <FormLabel>Is this for a new application?</FormLabel>
-        <Field name="isNewApp">
-          {({ field }) => (
-            <RadioGroup {...field} value={values.isNewApp === null ? '' : values.isNewApp.toString()}>
-              <HStack spacing={4}>
-                <Radio
-                  value="true"
-                  onChange={() => setFieldValue('isNewApp', true)}
-                >
-                  Yes
-                </Radio>
-                <Radio
-                  value="false"
-                  onChange={() => setFieldValue('isNewApp', false)}
-                >
-                  No
-                </Radio>
-              </HStack>
-            </RadioGroup>
-          )}
-        </Field>
-      </FormControl>
+          <CustomFormControl label="Is this for a new application?">
+            <Field name="isNewApp">
+              {({ field }) => (
+                <RadioGroup {...field}>
+                  <Radio value="true" mr={4}>Yes</Radio>
+                  <Radio value="false">No</Radio>
+                </RadioGroup>
+              )}
+            </Field>
+          </CustomFormControl>
 
-      <FormControl>
-        <FormLabel>Required Features</FormLabel>
-        <Field name="keyFeatures">
-          {({ field }) => (
-            <CheckboxGroup
-              {...field}
-              value={values.keyFeatures || []}
-              onChange={(values) => setFieldValue('keyFeatures', values)}
-            >
-              <SimpleGrid columns={3} spacing={2}>
-                {['SCALABILITY', 'CONSISTENCY', 'MULTI_REGION', 'DATA_COMPLIANCE', 'HIGH_AVAILABILITY', 'PERFORMANCE', 'MULTI_CLOUD'].map((feature) => (
-                  <Checkbox key={feature} value={feature}>
-                    {feature.replace('_', ' ')}
-                  </Checkbox>
-                ))}
-              </SimpleGrid>
-            </CheckboxGroup>
-          )}
-        </Field>
-      </FormControl>
+          <CustomFormControl label="Required Features">
+            <Field name="keyFeatures">
+              {({ field }) => (
+                <SimpleGrid columns={[2, 3, 4]} spacing={4}>
+                  {['SCALABILITY', 'CONSISTENCY', 'MULTI_REGION', 'DATA_COMPLIANCE', 'HIGH_AVAILABILITY', 'PERFORMANCE', 'MULTI_CLOUD'].map((feature) => (
+                    <Checkbox
+                      key={feature}
+                      {...field}
+                      value={feature}
+                      isChecked={field.value && field.value.includes(feature)}
+                      onChange={(e) => {
+                        const set = new Set(field.value || []);
+                        if (e.target.checked) {
+                          set.add(feature);
+                        } else {
+                          set.delete(feature);
+                        }
+                        setFieldValue('keyFeatures', Array.from(set));
+                      }}
+                    >
+                      {feature.replace('_', ' ')}
+                    </Checkbox>
+                  ))}
+                </SimpleGrid>
+              )}
+            </Field>
+          </CustomFormControl>
 
       <FormControl>
         <FormLabel>Minimum Acceptable Query Latency (ms)</FormLabel>
@@ -150,22 +154,20 @@ const ProspectForm = () => {
         </Field>
       </FormControl>
 
-      <FormControl isRequired>
-        <FormLabel>Project Completion Timeline?</FormLabel>
-        <Field name="timeline" as={Select}>
-          <option value="">Select a timeline</option>
-          <option value="0-3 months">0-3 months</option>
-          <option value="3-6 months">3-6 months</option>
-          <option value="6-12 months">6-12 months</option>
-          <option value="12+ months">12+ months</option>
-        </Field>
-      </FormControl>
+          <CustomFormControl label="Project Completion Timeline?" required>
+            <Field name="timeline" as={Select} placeholder="Select a timeline">
+              <option value="0-3 months">0-3 months</option>
+              <option value="3-6 months">3-6 months</option>
+              <option value="6-12 months">6-12 months</option>
+              <option value="12+ months">12+ months</option>
+            </Field>
+          </CustomFormControl>
 
-      <FormControl>
-        <FormLabel>Additional Notes</FormLabel>
-        <Field name="additionalNotes" as={Textarea} />
-      </FormControl>
-    </VStack>
+          <CustomFormControl label="Additional Notes">
+            <Field name="additionalNotes" as={Textarea} />
+          </CustomFormControl>
+        </VStack>
+      </Box>
   );
 };
 
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import { Box, VStack, Heading, Text, List, ListItem, Badge, SimpleGrid } from '@chakra-ui/react';
+import { Box, VStack, Heading, Text, List, ListItem, Badge, SimpleGrid, Flex, Circle } from '@chakra-ui/react';
 import { CheckIcon } from '@chakra-ui/icons';
 
 const CustomerStory = ({ story }) => (
-  <Box borderWidth="1px" borderRadius="lg" p={4} mb={4}>
-    <Heading size="md">{story.companyName}</Heading>
-    <Badge colorScheme="blue" mt={2}>{story.industry}</Badge>
-    <Text mt={2}><strong>Use Case:</strong> {story.useCase}</Text>
-    <Text mt={2}><strong>Challenge:</strong> {story.challenge}</Text>
-    <Text mt={2}><strong>Solution:</strong> {story.solution}</Text>
-    <SimpleGrid columns={2} spacing={2} mt={2}>
+  <Box
+    borderWidth="1px"
+    borderRadius="lg"
+    p={6}
+    mb={4}
+    boxShadow="md"
+    bg="white"
+    transition="all 0.3s"
+    _hover={{ boxShadow: "lg" }}
+  >
+    <Heading size="md" mb={2} color="blue.600">{story.companyName}</Heading>
+    <Badge colorScheme="blue" mb={3}>{story.industry}</Badge>
+    <Text mt={2} fontSize="sm"><strong>Use Case:</strong> {story.useCase}</Text>
+    <Text mt={2} fontSize="sm"><strong>Challenge:</strong> {story.challenge}</Text>
+    <Text mt={2} fontSize="sm"><strong>Solution:</strong> {story.solution}</Text>
+    <SimpleGrid columns={[2, 3, 4]} spacing={2} mt={4}>
       {story.keyFeatures.map((feature, index) => (
-        <Badge key={index} colorScheme="green">
-          <CheckIcon mr={1} />
-          {feature.replace('_', ' ')}
+        <Badge key={index} colorScheme="green" variant="subtle" borderRadius="full" px={3} py={1}>
+          <Flex align="center">
+            <CheckIcon mr={1} boxSize={3} />
+            <Text fontSize="xs">{feature.replace('_', ' ')}</Text>
+          </Flex>
         </Badge>
       ))}
     </SimpleGrid>
@@ -23,26 +34,43 @@ const CustomerStory = ({ story }) => (
 const Results = ({ data }) => {
   const { fitScore, customerStories } = data;
 
-  return (
-    <VStack spacing={6} align="stretch">
-      <Box>
-        <Heading size="lg">CockroachDB Fit Score</Heading>
-        <Text fontSize="4xl" fontWeight="bold" color={fitScore > 70 ? "green.500" : fitScore > 40 ? "yellow.500" : "red.500"}>
-          {fitScore}%
-        </Text>
-      </Box>
+  const getFitScoreColor = (score) => {
+    if (score > 70) return "green";
+    if (score > 40) return "yellow";
+    return "red";
+  };
 
-      <Box>
-        <Heading size="md">Matching Customer Stories</Heading>
-        <List spacing={4} mt={2}>
-          {customerStories.map((story, index) => (
-            <ListItem key={index}>
-              <CustomerStory story={story} />
-            </ListItem>
-          ))}
-        </List>
-      </Box>
-    </VStack>
+  return (
+    <Box maxW="4xl" mx="auto" bg="white" boxShadow="xl" rounded="lg" overflow="hidden">
+      <VStack spacing={8} align="stretch" p={8}>
+        <Box>
+          <Heading size="md" mb={4} textAlign="center">CockroachDB Fit Score</Heading>
+          <Flex justifyContent="center" alignItems="center">
+            <Circle
+              size="150px"
+              bg={`${getFitScoreColor(fitScore)}.100`}
+              borderWidth={8}
+              borderColor={`${getFitScoreColor(fitScore)}.500`}
+            >
+              <Text fontSize="4xl" fontWeight="bold" color={`${getFitScoreColor(fitScore)}.500`}>
+                {fitScore}%
+              </Text>
+            </Circle>
+          </Flex>
+        </Box>
+
+        <Box>
+          <Heading size="md" mb={4}>Matching Customer Stories</Heading>
+          <List spacing={6}>
+            {customerStories.map((story, index) => (
+              <ListItem key={index}>
+                <CustomerStory story={story} />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </VStack>
+    </Box>
   );
 };
 
